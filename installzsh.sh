a38713c08dd5495cbcc955825837bdfdb5416e47
Yuri Vidal <yurividal@users.noreply.github.com>
2023-05-07 18:58:08 +0100

    Create installzsh.sh

---
+++ installzsh.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# Install zsh
+sudo apt-get update
+sudo apt-get install -y zsh
+sudo apt install language-pack-en -y
+sudo apt install fonts-powerline -y
+
+# Set zsh as the default shell
+chsh -s $(which zsh)
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
+# Enable the agnoster theme
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"\nplugins=(git docker docker-compose zsh-autosuggestions)/g' ~/.zshrc
+
+echo 'DISABLE_LS_COLORS="false"' >> ~/.zshrc
+echo 'ENABLE_CORRECTION="true"' >> ~/.zshrc
+
+
+
+echo "export HISTCONTROL=ignoreboth:erasedups
+setopt EXTENDED_HISTORY 
+setopt HIST_EXPIRE_DUPS_FIRST 
+setopt HIST_IGNORE_DUPS 
+setopt HIST_IGNORE_ALL_DUPS 
+setopt HIST_IGNORE_SPACE 
+setopt HIST_FIND_NO_DUPS 
+setopt HIST_SAVE_NO_DUPS 
+setopt HIST_BEEP
+LS_COLORS='rs=0:di=00;34:ln=00;36:mh=00:pi=40;33:so=00;35:do=00;35:bd=40;33;01:cd=40;33;01:or=40;31;01:mi=00:su=37;41:sg=30;43:ca=30;41:tw=30;42:ow=34;42:st=37;44:ex=00;32:*.tar=00;31:*.tgz=00;31:*.arc=00;31:*.arj=00;31:*.taz=00;31:*.lha=00;31:*.lz4=00;31:*.lzh=00;31:*.lzma=00;31:*.tlz=00;31:*.txz=00;31:*.tzo=00;31:*.t7z=00;31:*.zip=00;31:*.z=00;31:*.Z=00;31:*.dz=00;31:*.gz=00;31:*.lrz=00;31:*.lz=00;31:*.lzo=00;31:*.xz=00;31:*.zst=00;31:*.tzst=00;31:*.bz2=00;31:*.bz=00;31:*.tbz=00;31:*.tbz2=00;31:*.tz=00;31:*.deb=00;31:*.rpm=00;31:*.jar=00;31:*.war=00;31:*.ear=00;31:*.sar=00;31:*.rar=00;31:*.alz=00;31:*.ace=00;31:*.zoo=00;31:*.cpio=00;31:*.7z=00;31:*.rz=00;31:*.cab=00;31:*.jpg=00;35:*.jpeg=00;35:*.mjpg=00;35:*.mjpeg=00;35:*.gif=00;35:*.bmp=00;35:*.pbm=00;35:*.pgm=00;35:*.ppm=00;35:*.tga=00;35:*.xbm=00;35:*.xpm=00;35:*.tif=00;35:*.tiff=00;35:*.png=00;35:*.svg=00;35:*.svgz=00;35:*.mng=00;35:*.pcx=00;35:*.mov=00;35:*.mpg=00;35:*.mpeg=00;35:*.m2v=00;35:*.mkv=00;35:*.webm=00;35:*.ogm=00;35:*.mp4=00;35:*.m4v=00;35:*.mp4v=00;35:*.vob=00;35:*.qt=00;35:*.nuv=00;35:*.wmv=00;35:*.asf=00;35:*.rm=00;35:*.rmvb=00;35:*.flc=00;35:*.avi=00;35:*.fli=00;35:*.flv=00;35:*.gl=00;35:*.dl=00;35:*.xcf=00;35:*.xwd=00;35:*.yuv=00;35:*.cgm=00;35:*.emf=00;35:*.ogv=00;35:*.ogx=00;35:*.aac=00;36:*.au=00;36:*.flac=00;36:*.m4a=00;36:*.mid=00;36:*.midi=00;36:*.mka=00;36:*.mp3=00;36:*.mpc=00;36:*.ogg=00;36:*.ra=00;36:*.wav=00;36:*.oga=00;36:*.opus=00;36:*.spx=00;36:*.xspf=00;36:';" >> ~/.zshrc
+
+echo "LS_COLORS='$LS_COLORS:ow=103;30;01'" >> ~/.zshrc                                            
+echo "export LS_COLORS" >> ~/.zshrc
+
+
+
+
+echo "ZSH_AUTOSUGGEST_STRATEGY=(match_prev_cmd history completion)
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE='fg=2'
+source ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
+
+rm ~/.zsh_history
+cp ~/.bash_history ~/.zsh_history
